0bcc7e5d8a75b8cb7859ab0862716664d7012fc4
Matheus Martins <matheushmar@gmail.com>
2023-08-23 08:17:47 -0300

    feat: 🎸 Add typing files

---
+++ types/drink.ts
@@ -0,0 +1,9 @@
+export interface Drink {
+  strDrink: string;
+  strDrinkThumb: string;
+  idDrink: string;
+}
+
+export interface DrinkData {
+  drinks: Drink[];
+}
---
+++ types/index.ts
@@ -0,0 +1,3 @@
+export * from './drink'
+export * from './drinkFull'
+export * from './drinkCategories'
