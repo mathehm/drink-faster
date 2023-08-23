01eb937e685857c373d4c7cd716020426144d9e4
Matheus Martins <matheushmar@gmail.com>
2023-08-23 08:17:17 -0300

    feat: 🎸 Add default layout

---
+++ layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="layout">
+    <HeaderComponent />
+    <NavComponent />
+    <div class="layout__content">
+      <slot />
+    </div>
+    <FooterComponent />
+  </div>
+</template>
+
+<style lang="scss">
+.layout {
+  font-family: $font-family;
+  display: grid;
+  grid-template-rows: min-content min-content auto 120px;
+  min-height: 100vh;
+}
+</style>
