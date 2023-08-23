21515b412bb4036c45846b74ebcf36ed8a903ef4
Matheus Martins <matheushmar@gmail.com>
2023-08-23 00:37:12 -0300

    first commit

---
+++ nuxt.config.ts
@@ -0,0 +1,40 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @import "@/assets/scss/_variables.scss";
+          @import "@/assets/scss/_colors.scss";
+          @import "@/assets/scss/_global.scss";
+          @import "@/assets/scss/_breakpoints.scss";
+          @import "@/assets/scss/_transitions.scss";
+          `,
+        },
+      },
+    },
+  },
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'pt-BR',
+      },
+      link: [
+        {
+          rel: 'stylesheet',
+          type: 'text/css',
+          href: 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined',
+        },
+        {
+          rel: 'stylesheet',
+          type: 'text/css',
+          href: 'https://fonts.googleapis.com/icon?family=Material+Icons',
+        },
+      ],
+    },
+    pageTransition: { name: 'fade', mode: 'out-in' },
+  },
+  ssr: false,
+})
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    main
+  </main>
+</template>
+
+<style lang="scss" scoped>
+.content {
+  height: 4rem;
+  width: 100%;
+  color: $white;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
